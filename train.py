48f0e46304d8acda0b765b3f23c60f55d07367f5
David Fan <dfan@princeton.edu>
2019-01-24 22:53:32 -0500

    Test time augmentation (3x)

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ class WhalesDataset(data.dataset.Dataset):
     if self.is_train:
       whale_id = train_labels_map[train_csv.iloc[index]['Id']] # [] is loc so use iloc!!!
       img_path = train_csv.iloc[index]['Path']
-      img = Image.open(img_path).convert('RGB')
+      img = Image.open(img_path)
       if self.transform:
         img = self.transform(img) # ToTensor converts (HxWxC) -> (CxHxW)
       return index, whale_id, img
@@ -39,15 +39,12 @@ class WhalesDataset(data.dataset.Dataset):
 def train():
   num_classes = len(train_labels)
   # Hyperparameters
-  num_epochs = 40
+  num_epochs = 25
   first_learning_rate = 0.001
   second_learning_rate = 0.00015
   train_params = {'batch_size': 20, 'shuffle': True, 'num_workers': 5}
   test_params = {'batch_size': 20, 'shuffle': True, 'num_workers': 5}
   train_valid_params = {'batch_size': 40, 'shuffle': True, 'num_workers': 5}
-  
-  imagenet_mean = [0.485, 0.456, 0.406]
-  imagenet_std = [0.229, 0.224, 0.225]
 
   # Load Data
   train_steps = transforms.Compose([
@@ -55,24 +52,33 @@ def train():
     transforms.RandomRotation(15),
     transforms.RandomHorizontalFlip(),
     transforms.ColorJitter(brightness=0.3),
-    # transforms.Grayscale(num_output_channels=3),
+    transforms.Grayscale(num_output_channels=3),
     transforms.CenterCrop(224), # ImageNet standard
     transforms.ToTensor(),
     transforms.Normalize(mean=imagenet_mean, std=imagenet_std)
   ])
 
-  test_process_steps = transforms.Compose([
-    # transforms.Grayscale(num_output_channels=3),
+  test_steps = transforms.Compose([
+    transforms.ToPILImage(),
     transforms.Resize(256),
+    transforms.Grayscale(num_output_channels=3),
     transforms.CenterCrop(224), # to handle images larger, have to modify last layer's input features
     transforms.ToTensor(),
     # Necessary for pre-trained models ...
     transforms.Normalize(mean=imagenet_mean, std=imagenet_std)
   ])
+
+  # Only convert to tensor (for test time augmentation)
+  test_load = transforms.Compose([
+    transforms.Resize(256),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.CenterCrop(256),
+    transforms.ToTensor()
+  ])
     
   train_set = WhalesDataset(is_train = True, transform=train_steps)
-  train_valid_set = WhalesDataset(is_train = True, transform=test_process_steps)
-  test_set = WhalesDataset(is_train = False, transform=test_process_steps)
+  train_valid_set = WhalesDataset(is_train = True, transform=test_load)
+  test_set = WhalesDataset(is_train = False, transform=test_load)
   train_loader = data.DataLoader(train_set, **train_params)
   train_valid_loader = data.DataLoader(train_valid_set, **train_valid_params)
   test_loader = data.DataLoader(test_set, **test_params)
@@ -106,8 +112,8 @@ def train():
       whale_ids = torch.tensor(np.array(whale_ids)).to(device)
       whale_ids.cuda()
       # Send tensors to GPU
-      # whale_ids = whale_ids.to(device) # batch_size x 3 x 128 x 128
-      images = images.to(device)   # batch_size x 128 x 128
+      # whale_ids = whale_ids.to(device) # batch_size x 1
+      images = images.to(device)   # batch_size x 3 x 224 x 224
       model.cuda()
       images.cuda()
       model.train() # reset model to training mode
@@ -115,7 +121,6 @@ def train():
       # Forward pass
       outputs = model(images)
       loss = criterion(outputs, whale_ids)
-      #loss = -train_acc(model, train_loader, num_iters = 10)
       # use backward() to do backprop on loss variable
       optimizer.zero_grad()
       loss.backward()
@@ -127,14 +132,15 @@ def train():
         losses.append(loss.item())
         print ('Epoch [{}/{}], Step [{}/{}], Batch Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_steps, loss.item()))
         sys.stdout.flush()
-    curr_acc = train_acc(model, train_valid_loader, num_iters = 10)
-    print('Approx. training accuracy: {}'.format(curr_acc))
+    if (epoch + 1) % 5 == 0:
+      curr_acc = train_acc(model, train_valid_loader, test_steps, num_iters = 5)
+      print('Approx. training accuracy: {}'.format(curr_acc))
   
   # Calculate loss over larger subset of training set instead of batch
-  final_acc = train_acc(model, train_valid_loader, num_iters = 40)
+  final_acc = train_acc(model, train_valid_loader, test_steps, num_iters = 20)
   print('Final training subset accuracy: {}'.format(final_acc))
   print('Making predictions on testing set:')
-  make_predictions(model, test_loader)
+  make_predictions(model, test_loader, test_steps)
 
 def map_for_image(label, predictions):
   """Computes the precision score of one image.
@@ -151,20 +157,54 @@ def map_for_image(label, predictions):
   except ValueError:
     return 0.0
 
-def train_acc(model, loader, num_iters):
+# Return augmented images for TTA at validation/prediction time
+def augment_images(images, test_steps = None):
+  augmentation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(256),
+    transforms.RandomRotation(15),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.3),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=imagenet_mean, std=imagenet_std)
+  ])
+  if test_steps is not None:
+    augmentation = test_steps
+  new_images = torch.zeros([images.shape[0], 3, 224, 224])
+  for i in range(0, images.shape[0]):
+    new_images[i] = augmentation(images[i])
+  return new_images
+
+def train_acc(model, loader, test_steps, num_iters):
   # Test the mdoel
   model.eval()
   
   acc = 0.0
   iters = 0
   with torch.no_grad():
-    for _, whale_ids, images in loader:
-      images = images.cuda()
+    for _, whale_ids, images_orig in loader:
+      # Just cropping
+      images1 = augment_images(images_orig, test_steps)
+      # Two augmentations
+      images2 = augment_images(images_orig)
+      images3 = augment_images(images_orig)
+      images1 = images1.cuda()
+      images2 = images2.cuda()
+      images3 = images3.cuda()
       batch_size = len(whale_ids)
       whale_ids = torch.tensor(np.array(whale_ids)).cuda()
       model.cuda()
-      outputs = nn.functional.softmax(model(images), 1)
+
+      # Original unaugmented image (except cropped)
+      output1 = model(images1)
+      output2 = model(images2)
+      output3 = model(images3)
+      outputs = (output1 + output2 + output3) / 3
+      outputs = nn.functional.softmax(outputs, 1)
       probs, predicted = torch.topk(outputs.data, 5, dim=1) # batch_size x 5
+      
       for i in range(predicted.shape[0]):
         true_label = train_labels[whale_ids[i]]
         predicted_labels = [train_labels[predicted[i, j]] for j in range(5)]
@@ -180,15 +220,28 @@ def train_acc(model, loader, num_iters):
     model.train() # reset model
     return acc / batch_size / iters
 
-def make_predictions(model, loader):
+def make_predictions(model, loader, test_steps):
   model.eval() # eval mode
   with torch.no_grad():
-    results = [['Image', 'Id']]
+    results = []
     seenImg = {}
-    for _, image_paths, images in loader:
-      images = images.cuda()
+    for _, image_paths, images_orig in loader:
+      # Just cropping
+      images1 = augment_images(images_orig, test_steps)
+      # Two augmentations
+      images2 = augment_images(images_orig)
+      images3 = augment_images(images_orig)
+      images1 = images1.cuda()
+      images2 = images2.cuda()
+      images3 = images3.cuda()
       model.cuda()
-      outputs = nn.functional.softmax(model(images), 1)
+
+      # Original unaugmented image (except cropped)
+      output1 = model(images1)
+      output2 = model(images2)
+      output3 = model(images3)
+      outputs = (output1 + output2 + output3) / 3
+      outputs = nn.functional.softmax(outputs, 1)
       probs, predicted = torch.topk(outputs.data, 5, dim=1) # batch_size x 5
      
       for i in range(predicted.shape[0]):
@@ -204,6 +257,8 @@ def make_predictions(model, loader):
           results.append(row)
           seenImg[image_paths[i]] = True
     model.train() # reset
+    results.sort() # sort by filename (the first substring of each row)
+    results.insert(0, ['Image', 'Id'])
     with open(test_csv_path, 'w') as predictionFile:
       wr = csv.writer(predictionFile)
       wr.writerows(results)
@@ -216,6 +271,8 @@ if __name__ == '__main__':
   test_csv_path = os.path.abspath('./predictions.csv')
 
   new_whale_thresh = 0.4
+  imagenet_mean = [0.485, 0.456, 0.406]
+  imagenet_std = [0.229, 0.224, 0.225]
 
   # Read train.csv into Pandas dataframe
   train_csv = pd.read_csv(csv_path)
